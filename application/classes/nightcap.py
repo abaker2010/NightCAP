eccd63993d7522ffe0ff0734e680d9dff87183e0
Aaron Baker <crosby.baker2010@gmail.com>
2020-12-23 10:56:47 -0700

    Intermediate update for updater
    Intermediate update for the updater. Removing most of the pcap files to save space since they are un-needed for the most part

--- application/classes/nightcap.py
+++ application/classes/nightcap.py
@@ -8,6 +8,7 @@ from application.classes.configuration.configuration import Configuration
 from application.classes.options.dynamic_options import NightcapDynamicOptions
 from application.classes.settings.settings import NightcapSettings
 from application.classes.helpers.screen.screen_helper import ScreenHelper
+from application.classes.updater.updater import  NightcapUpdater
 #endregion
 
 class Nightcap(NightcapDynamicOptions):
@@ -19,4 +20,9 @@ class Nightcap(NightcapDynamicOptions):
         print("Settings here")
         ScreenHelper().clearScr()
         NightcapSettings().cmdloop()
+
+    def do_update(self, line):
+        print("Updating system")
+        NightcapUpdater().update()
+
 #endregion
