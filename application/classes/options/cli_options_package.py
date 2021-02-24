938bd86a58b21a94b73a7aab81ae95090aa77093
Aaron Baker <crosby.baker2010@gmail.com>
2021-02-24 14:49:55 -0700

    Update cli_options_package.py
    Update to the package object so that it has the params option. This needs more work but saving some of the old code

--- application/classes/options/cli_options_package.py
+++ application/classes/options/cli_options_package.py
@@ -3,13 +3,79 @@
 # This file is part of the Nightcap Project,
 # and is released under the "MIT License Agreement". Please see the LICENSE
 # file that should have been included as part of this package.
+import os
+import copy
+import json
 from nightcapcore import configuration
 from nightcapcore.base import NightcapCoreBase
+from nightcapcore.params import NightcapDynamicParams
 from application.classes.base_cmd.base_cmd import NightcapBaseCMD
+from colorama import Fore, Style
 
 class NightcapCLIOptionsPackage(NightcapBaseCMD):
     def __init__(self,selectedList: list, configuration: configuration, packagebase: NightcapCoreBase = NightcapCoreBase()):
         NightcapBaseCMD.__init__(self, selectedList, configuration, packagebase)
+        self.config = configuration
+        try:
+            self.package_params = copy.deepcopy(self.packages_db.package_params(self.selectedList))
+        except Exception as e:
+            self.package_params = None
+
+    def help_params(self):
+        h1 = "See all available parameters:"
+        h2 = "params"
+        h3 = "set param:\tparams [PARAM] [PARAMVALUE]"
+        p = '''
+         %s 
+         %s
+         %s
+        ''' % (
+            (Fore.GREEN + h1),
+            (Fore.YELLOW + h2 + Style.RESET_ALL),
+            (Fore.YELLOW + h3 + Style.RESET_ALL),
+            )
+        print(p)
 
     def do_params(self, line):
         print("Find out package params")
+        print("Base package")
+
+        if(len(line) == 0):
+            title1 = "Base Params"
+            self.console_output.output("\n")
+            self.console_output.output(title1, color=Fore.CYAN)
+            self.console_output.output("-"*(len(title1)*2), color=Fore.LIGHTYELLOW_EX)
+            self.package_base.show_params()
+        
+
+
+    def do_run(self,line):
+    
+        force = False
+        if(self.package_base.project == None):
+            force = input((Fore.YELLOW + "Project not selected to be used would you like to continue? [Y/n]: " + Fore.GREEN))
+            print(Style.RESET_ALL, Fore.LIGHTCYAN_EX)
+            yes_options = self.config.Config()["NIGHTCAP"]["yes"].split(" ")
+            if(force == None):
+                force = False
+            else:
+                if(force in yes_options):
+                    force = True
+        else:
+            force = True
+
+        if(force == True):
+            if(len(self.selectedList) == 3):
+                exe_path = self.packages_db.get_package_run_path(self.selectedList)
+                dat = {}
+                dat[0] = self.package_base.toJson()
+                dat[1] = self.package_params
+                print("data before passing: ", dat)
+                
+                call = "python3.8 %s --data '%s'" % (exe_path, json.dumps(dat))
+                
+                os.system(call)
+            else:
+                print("Package not selected to be used")
+        else:
+            self.console_output.output("Scan canceled by user\n", level=6)
